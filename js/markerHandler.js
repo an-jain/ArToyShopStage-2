05c7ba48147db62dbcec4fd16e5e9dc18d4c2a27
an-jain <89581293+an-jain@users.noreply.github.com>
2023-05-11 15:50:22 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,42 @@
+AFRAME.registerComponent("markerHandler", {
+    init: async function() {
+        this.el.addEventListener("markerFound", () => {
+            console.log("marker is found");
+            this.handleMarkerFound();
+        })
+        this.el.addEventListener("markerLost", () => {
+            console.log("marker is lost");
+            this.handleMarkerFound();
+        })
+    },
+
+    handleMarkerFound: function() {
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "flex";
+
+        var orderButton = document.getElementById("order-button");
+        var orderSummaryButton = document.getElementById("order-summary-button");
+
+        orderButton.addEventListener("click", () => {
+            swal({
+                icon: "https://i.imgur.com/4NZ6uLY.jpg",
+                title: "Thanks for your Order",
+                text: "",
+                timer: 2000,
+                buttons: false
+            });
+        });
+       orderSummaryButton.addEventListener("click", () => {
+        swal({
+            icon: "warning",
+            title: "orderSummary",
+            text: "Work in Progress"
+        })
+       })
+    },
+
+    handleMarkerLost: function() {
+        var buttonDiv = document.getElementById("button-div");
+        buttonDiv.style.display = "none";
+    }
+});
